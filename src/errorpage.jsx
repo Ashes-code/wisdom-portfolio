8d45a93dc333f0a0c96cadbc8ca1c897e925a9a4
Wisdom Ezeh <wisdomcezeh@gmail.com>
2025-01-17 14:25:45 +0100

    first portfolio commit

---
+++ src/errorpage.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+  return(
+    <div className='bg-black text-white font-black text-4xl text-center flex flex-col gap-9 justify-center align-center h-[100vh] w-full'>404 Not Found
+      <a href="/" className="text-blue-300 underline text-xl">Go to Home</a>
+    </div>
+  );
+}
