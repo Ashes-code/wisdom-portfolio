13974f419930a76215fdc1ecf22f886501a9266b
Wisdom Ezeh <wisdomcezeh@gmail.com>
2025-03-08 05:06:19 +0100

    Correction on landing page

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -1,14 +1,16 @@
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import GSAPDemo from "./gsap";
 import App from "./App.jsx";
+
+const BASE_PATH = import.meta.env.VITE_BASE_PATH || "";
 
 const Navigation = () => {
   return (
     <Routes>
-      <Route path="/wisdom-portfolio/" element={<GSAPDemo />} />
-      <Route path="/wisdom-portfolio/home" element={<App />} />
+      <Route path={`${BASE_PATH}/`} element={<GSAPDemo />} />
+      <Route path={`${BASE_PATH}/home`} element={<App />} />
     </Routes>
   );
-}
+};
 
-export default Navigation
+export default Navigation;
