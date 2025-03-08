9b8b84319d8d9f97f0f3b2b230187288b6874980
Wisdom Ezeh <wisdomcezeh@gmail.com>
2025-03-08 05:47:44 +0100

    End commit for animations

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? "/" : "/wisdom-portfolio/",
-  define: {
-    "import.meta.env.VITE_BASE_PATH": JSON.stringify(
-      process.env.VERCEL ? "" : "/wisdom-portfolio"
-    ),
-  },
-});
+  base: process.env.VERCEL ? '/' : '/wisdom-portfolio/',
+})
