33f960e1f2cc427bce4201b8babe3e183182a309
Wisdom Ezeh <wisdomcezeh@gmail.com>
2025-06-26 11:30:31 +0100

    added link to gmail and whatsapp

--- src/contact.jsx
+++ src/contact.jsx
@@ -22,20 +22,40 @@ const Contact = () => {
         </p>
       </div>
       <div className="flex flex-col items-center text-center pb-12 gap-6">
-        <motion.h1 initial={{opacity: 0, y: -30}}
-        whileInView={{opacity: 1, y: 0}}
-        transition={{duration: 1}}
-        viewport={{ once: true }}className="font-bold text-lg md:text-2xl lg:text-3xl text-white flex items-center animate-three">
-          <img src={email} alt="Email Icon" className="h-6 mr-2" />
-          wisdomcezeh@gmail.com
-        </motion.h1>
-        <motion.h1 initial={{opacity: 0, y: -30}}
-        whileInView={{opacity: 1, y: 0}}
-        transition={{duration: 1}}
-        viewport={{ once: true }}className="font-bold text-lg md:text-2xl lg:text-3xl text-white flex items-center animate-three">
-          <img src={phone} alt="Phone Icon" className="h-6 mr-2" />
-          +234 7072967842
-        </motion.h1>
+        <a
+        href="https://mail.google.com/mail/?view=cm&fs=1&to=wisdomcezeh@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          <motion.h1
+            initial={{ opacity: 0, y: -30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+            className="font-bold text-lg md:text-2xl lg:text-3xl text-white flex items-center animate-three hover:text-blue-700"
+            >
+            <img src={email} alt="Email Icon" className="h-6 mr-2" />
+            wisdomcezeh@gmail.com
+            </motion.h1>
+          </a>
+
+          <a
+            href="https://wa.me/2347072967842"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.h1
+              initial={{ opacity: 0, y: -30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              viewport={{ once: true }}
+              className="font-bold text-lg md:text-2xl lg:text-3xl text-white flex items-center animate-three hover:text-blue-700"
+            >
+              <img src={phone} alt="Phone Icon" className="h-6 mr-2" />
+              +234 7072967842
+            </motion.h1>
+          </a>
+
       </div>
       <div>
         <p className="text-white text-lg font-semibold text-center mb-4">
